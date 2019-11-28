251dd7b0a32f4630565f46419fb9afc2ae354230
kalzpi <48274440+kalzpi@users.noreply.github.com>
2019-11-28 12:15:17 +0900

    FBV_or_CBV?
    What a magical thing!

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,4 @@
-from django.urls import reverse
-from django.http import Http404
-from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -16,11 +13,5 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        print(vars(room))
-        return render(request, "rooms/detail.html", context={"room": room})
-    except models.Room.DoesNotExist:
-        # return redirect(reverse("core:home"))
-        raise Http404()
+class RoomDetail(DetailView):
+    model = models.Room
