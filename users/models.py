797728dceb66460b1362924ec382474b38bcd21a
kalzpi <48274440+kalzpi@users.noreply.github.com>
2019-12-04 15:58:46 +0900

    ModelForm
    Learned how to use modelform.
    How to override save().
    the meaning of commit=false

--- users/models.py
+++ users/models.py
@@ -32,8 +32,12 @@ class User(AbstractUser):
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(blank=True, null=True)
 
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_KOREAN
+    )
 
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW
+    )
 
     superhost = models.BooleanField(default=False)
--- users/views.py
+++ users/views.py
@@ -28,7 +28,7 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {"first_name": "Yunho", "last_name": "Huh", "email": "yhhuh@srcroll.com"}
+    initial = {}
 
     def form_valid(self, form):
         form.save()
